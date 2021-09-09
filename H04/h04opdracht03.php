022f015a84d0bf1d0c9344e555b50262b2b9ab78
LynnvD <lvandam@roc-dev.com>
2021-09-09 12:59:22 +0200

    hoofdstuk 4, opdracht 3 gemaakt en getest

--- H04/h04opdracht03.php
+++ H04/h04opdracht03.php
@@ -1,1 +1,12 @@
 <?php
+
+function reverse($a) {
+    $b = "";
+    $word = strlen($a) -1;
+    for ($i = $word; $i >= 0; $i--) {
+        $b .= $a[$i];
+    }
+    return $b;
+}
+
+echo reverse("ereht olleh");
