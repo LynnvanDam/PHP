60fa95de433e88635536ae136fc98e662547441f
LynnvD <lvandam@roc-dev.com>
2021-09-16 14:33:18 +0200

    Hoofdstuk 5, opdracht 1 afgemaakt

--- H05/h05opdracht01.php
+++ H05/h05opdracht01.php
@@ -1,7 +1,21 @@
 <?php
 
-echo $_POST['inlognaam'];
-    echo "<br>";
-echo $_POST['adres'];
-    echo "<br>";
-echo $_POST['email'];
+if ($_POST['inlognaam'] == "") {
+    echo "vul een naam in you big dumb";
+    echo "<br> <a href='opdr01form1.html'>Terug naar formulier :)</a>";
+}
+if ($_POST['adres'] == "") {
+    echo "vul een adres in dummy";
+    echo "<br> <a href='opdr01form1.html'>Terug naar formulier :)</a>";
+}
+if ($_POST['email'] == "") {
+    echo "vul een email in *deep sigh*";
+    echo "<br> <a href='opdr01form1.html'>Terug naar formulier :)</a>";
+}
+if ($_POST['wachtwoord'] == "") {
+    echo "vul een wachtwoord in";
+    echo "<br> <a href='opdr01form1.html'>Terug naar formulier :)</a>";
+}
+    echo $_POST['inlognaam']."<br>";
+    echo $_POST['adres']."<br>";
+    echo $_POST['email'];
